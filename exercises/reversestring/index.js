1b0378d0e96b5ac8cee8d9ae4b5359c7194b0720
github username <ianjpollack@gmail.com>
2018-11-11 00:18:27 -0500

    reverse a string complete

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -11,12 +11,27 @@ function reverse(str) {
   return str.split("").reverse().join("")
 }
 
-// SOLUTION #1
-function reverse(str) {
-  const arr = str.split('');
-  arr.reverse();
-  return arr.join('');
-}
+// // SOLUTION #1
+// function reverse(str) {
+//   const arr = str.split('');
+//   arr.reverse();
+//   return arr.join('');
+// }
 
+// SOLUTION #2
+// function reverse(str) {
+//   let reversed = "";
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+// SOLUTION #3
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '');
+// }
 
 module.exports = reverse;
