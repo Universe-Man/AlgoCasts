a1c4a50f334aac3fa372e0db057f949dd8e6c248
github username <ianjpollack@gmail.com>
2018-11-11 00:51:31 -0500

    palindrome complete

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -17,4 +17,18 @@ function palindrome(str) {
   }
 }
 
+// SOLUTION #1
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('');
+//   return str === reversed;
+// }
+
+// SOLUTION #2
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     return char === str[str.length - i - 1]
+//   })
+// }
+
+
 module.exports = palindrome;
