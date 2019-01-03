43aec722b3f243342f8c2759244541da983eb0f1
github username <ianjpollack@gmail.com>
2019-01-03 01:00:14 -0500

    fizzbuzz 1st attempt (failed)

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -22,7 +22,15 @@
 // )
 function fizzBuzz(n) {
   for (i = 0; i < n; ++i) {
-    console.log(i);
+    if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else {
+      console.log(i);
+    }
   }
 }
 
