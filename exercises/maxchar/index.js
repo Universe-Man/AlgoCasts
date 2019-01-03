27ea8eb52bbac0793adf85baef939598434d8ce3
github username <ianjpollack@gmail.com>
2019-01-03 00:28:01 -0500

    charMax solution part 1

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  console.log(charMap);
+
+}
+
+// FIRST GO AT IT
+// function maxChar(str) {
+//   let chars = {};
+//   for (let char of str) {
+//     if (!chars[char]) {
+//       chars[char] = 1;
+//     } else {
+//       chars[char]++;
+//     }
+//   }
+//   let keys = Object.keys(chars);
+//   let values = Object.values(chars);
+//
+//
+// }
 
 module.exports = maxChar;
