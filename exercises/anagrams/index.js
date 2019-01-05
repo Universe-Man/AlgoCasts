ce05927902591896f0d83702fbf365d7a1248bef
github username <ianjpollack@gmail.com>
2019-01-04 21:31:27 -0500

    anagrams first stab part 1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,32 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const wordA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  const wordB = stringB.replace(/[^\w]/g, "").toLowerCase();
+  let objA = {};
+  let objB = {};
+  // if (wordA.length !== wordB.length) {
+    // return false;
+  // } else {
+  for (letter of wordA) {
+    if (Object.keys(objA).includes(letter)) {
+      objA[letter]++;
+    } else {
+      objA[letter] = 1;
+    }
+    // }
+  }
+  for (letter of wordB) {
+    if (Object.keys(objB).includes(letter)) {
+      objB[letter]++;
+    } else {
+      objB[letter] = 1;
+    }
+  }
+  // console.log("AAAA", objA);
+  // console.log("BBBB", objB);
+  Boolean();
+}
 
 module.exports = anagrams;
