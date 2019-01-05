d185c508e6c20d6ef3635cd0e81122c2159e45b7
github username <ianjpollack@gmail.com>
2019-01-05 03:56:26 -0500

    first solution complete (i may have been really close with a previous solution of mine by just forgetting to put return true at the end, not sure though

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -11,6 +11,15 @@
 function anagrams(stringA, stringB) {
   const aCharMap = buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
+  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // USE HELPER FUNCTIONS
