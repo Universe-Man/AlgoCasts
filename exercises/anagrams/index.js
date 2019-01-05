61aa600c66711b15e506305a36c86f8b7e7c4e34
github username <ianjpollack@gmail.com>
2019-01-04 22:04:39 -0500

    anagram complete FAILURE couldn't get it to work and want to kill myself now, goodbye cruel world

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -33,7 +33,38 @@ function anagrams(stringA, stringB) {
   }
   // console.log("AAAA", objA);
   // console.log("BBBB", objB);
-  Boolean();
+  let sortedObjA = {};
+  let sortedObjB = {};
+  /// NEEDED TO SORT THE KEYS (AND VALUES) OF OBJECT [TAKEN FROM STACK OVERFLOW]
+  Object.keys(objA).sort().forEach((key) => {
+    sortedObjA[key] = objA[key];
+  })
+  Object.keys(objB).sort().forEach((key) => {
+    sortedObjB[key] = objB[key];
+  })
+  ///
+  // Boolean(sortedObjA == sortedObjB);
+  // if (sortedObjA === sortedObjB) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  // let A = JSON.stringify(sortedObjA);
+  // let B = JSON.stringify(sortedObjB);
+  // Boolean(A === B);
+  // I THOUGHT I WAS REALLY ON TO SOMETHING BUT I JUST CAN'T GET THIS COMPARISON TO WORK. I KNOW IF HAS TO DO WITH THE FACT THAT OBJs REFER TO THE LOCATION OF THE DATA, SO THE 2 OBJECTS, WHILE IDENTICAL, ARE NOT THE SAME.
+  // LAST ATTEMPT AT MERGING KEYS AND VALUES INTO CUSTOM ARRAY
+  let Akeys = Object.keys(sortedObjA)
+  let Avalues = Object.values(sortedObjA);
+  let Bkeys = Object.keys(sortedObjB)
+  let Bvalues = Object.values(sortedObjB);
+  let A = Akeys + Avalues;
+  let B = Bkeys + Bvalues;
+  console.log("AAAA", A);
+  console.log("BBBB", B);
+  Boolean(A === B);
 }
+
+// THIS WAS PRETTY SHITTY BECAUSE I'M GETTING A TYPEERROR INSISTING THAT OBJECT.VALUES() ISN'T A FUNCTION WHEN INFACT I'M 100% SURE THAT IT IS BECAUSE I'M STARING DIRECTLY AT THE OBJECT.VALUES() MDN PAGE! AND EVEN WHEN I FIBBED IT, IT STILL DIDN'T WORK. SUPER BUMMED BUT IT'S CLEAR I HAVE NO IDEA HOW TO SOLVE THIS. TIME TO JUST GET THE SOLUTION AND MOVE ON, BUT DEFINETLY FRUSTRATED I CAME UP EMPTY HANDED FOR THE MOST PART.
 
 module.exports = anagrams;
